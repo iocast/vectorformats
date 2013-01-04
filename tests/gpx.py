6ca7404c5521a10ecd8d4ae84fd0a3f31861a583
Yohan Boniface <yb@enix.org>
2013-01-04 18:27:51 +0100

    First shot in managing GPX decode

---
+++ tests/gpx.py
@@ -0,0 +1,37 @@
+import unittest
+
+from vectorformats.formats.gpx import GPX
+
+from base import BaseTest
+
+
+class GPXDecodeTest(BaseTest):
+
+    def decode(self, filename, **kwargs):
+        """
+        Process a file an return features.
+        """
+        content = self.read_file(filename)
+        gpx = GPX(**kwargs)
+        return gpx.decode(content)
+
+    def test_decode_simple_point(self):
+        features = self.decode("simple_point.gpx")
+        self.assertEqual(len(features), 1)
+        point = features[0]
+        self.assertEqual(point.properties['name'], 'Simple Point')
+        self.assertEqual(point.properties['desc'], 'Simple description')
+        self.assertEqual(point.geometry['type'], 'Point')
+        self.assertEqual(point.geometry['coordinates'], [-121.72904, 45.44283, 1374.0])
+
+    def test_decode_simple_path(self):
+        features = self.decode("simple_path.gpx")
+        self.assertEqual(len(features), 1)
+        path = features[0]
+        self.assertEqual(path.properties['name'], 'Simple path')
+        self.assertEqual(path.properties['desc'], 'Simple description')
+        self.assertEqual(path.geometry['type'], 'LineString')
+        self.assertEqual(path.geometry['coordinates'], [[-121.7295456, 45.4431641], [-121.7290800, 45.4428615], [-121.7279085, 45.4425697]])
+
+if __name__ == '__main__':
+    unittest.main()
--- vectorformats/formats/gpx.py
+++ vectorformats/formats/gpx.py
@@ -7,43 +7,45 @@ Created on Jul 30, 2011
 import types
 
 from xml.sax.saxutils import escape
+from xml.dom import minidom
 
+from ..feature import Feature
 from .format import Format
 
 
 class GPX(Format):
-    
+
     def encode(self, features, **kwargs):
         results = ["""<?xml version="1.0" encoding="UTF-8"?>
         <gpx version="1.0">"""]
-        results.append("<name>%s</name>" % self.layername) 
-        
+        results.append("<name>%s</name>" % self.layername)
+
         for feature in features:
             results.append(self.encode_feature(feature))
-        
+
         results.append("</gpx>")
         return "\n".join(results)
-        
+
     def encode_feature(self, feature):
         xml = []
-        
+
         if feature.geometry['type'] == 'Point':
             xml.append("""<wpt lon="%s" lat="%s">""" % (str(feature.geometry["coordinates"][0]), str(feature.geometry["coordinates"][1])))
-            if feature.properties.has_key('name'):
+            if "name" in feature.properties:
                 if isinstance(feature.properties["name"], types.NoneType):
                     xml.append("""<name>%s</name>""" % str(feature.id))
                 else:
                     xml.append("""<name>%s</name>""" % escape(feature.properties["name"]))
             else:
                 xml.append("""<name>%s</name>""" % str(feature.id))
-            if feature.properties.has_key('ele'):
+            if "ele" in feature.properties:
                 xml.append("""<ele>%s</ele>""" % feature.properties["ele"])
             xml.append("""</wpt>""")
 
         elif feature.geometry['type'] == 'LineString':
             xml.append("<trk>")
-            
-            if feature.properties.has_key('name'):
+
+            if "name" in feature.properties:
                 if isinstance(feature.properties["name"], types.NoneType):
                     xml.append("""<name>%s</name>""" % str(feature.id))
                 else:
@@ -56,37 +58,89 @@ class GPX(Format):
             coords = feature["geometry"]["coordinates"]
             for coord in coords:
                 xml.append("""<trkpt lon="%s" lat="%s">""" % (str(coord[0]), str(coord[1])))
-                
-                if feature.properties.has_key('ele'):
+
+                if "ele" in feature.properties:
                     xml.append("""<ele>%s</ele>""" % feature.properties["ele"])
-                
+
                 xml.append("</trkpt>")
 
             xml.append("</trkseg></trk>")
-            
+
         elif feature.geometry['type'] == 'Polygon':
             xml.append("<trk>")
-            
-            if feature.properties.has_key('name'):
+
+            if "name" in feature.properties:
                 if isinstance(feature.properties["name"], types.NoneType):
                     xml.append("""<name>%s</name>""" % str(feature.id))
                 else:
                     xml.append("""<name>%s</name>""" % escape(feature.properties["name"]))
             else:
                 xml.append("""<name>%s</name>""" % str(feature.id))
-            
+
             xml.append("<trkseg>")
-            
+
             coords = feature["geometry"]["coordinates"][0]
             for coord in coords:
                 xml.append("""<trkpt lon="%s" lat="%s">""" % (str(coord[0]), str(coord[1])))
-                
-                if feature.properties.has_key('ele'):
+
+                if "ele" in feature.properties:
                     xml.append("""<ele>%s</ele>""" % feature.properties["ele"])
-                
+
                 xml.append("</trkpt>")
-            
+
             xml.append("</trkseg></trk>")
 
         return "\n".join(xml)
 
+    def decode(self, data):
+        features = []
+
+        doc = minidom.parseString(data)
+        for node in doc.documentElement.childNodes:
+            if node.nodeType != doc.ELEMENT_NODE:
+                continue
+            feature = self.node_to_feature(node)
+            features.append(feature)
+
+        return features
+
+    def node_to_feature(self, node):
+        feature = Feature()
+        if node.nodeName == "wpt":
+            coords = self.node_to_coordinates(node)
+            feature.geometry = {
+                'type': 'Point',
+                'coordinates': coords
+            }
+        elif node.nodeName == "trk":
+            coords = []
+            for subnode in node.getElementsByTagName('trkseg')[0].childNodes:
+                if subnode.nodeType != node.ELEMENT_NODE:
+                    continue
+                coords.append(self.node_to_coordinates(subnode))
+            feature.geometry = {
+                'type': 'LineString',
+                'coordinates': coords
+            }
+        self.populate_property_from_node(node, feature, "name")
+        self.populate_property_from_node(node, feature, "desc")
+        self.populate_property_from_node(node, feature, "cmt")
+        self.populate_property_from_node(node, feature, "sym")
+        return feature
+
+    def node_to_coordinates(self, node):
+        lat = float(node.getAttribute('lat'))
+        lon = float(node.getAttribute('lon'))
+        coords = [lon, lat]
+        if node.getElementsByTagName('ele'):
+            ele = float(node.getElementsByTagName('ele')[0].firstChild.nodeValue)
+            coords.append(ele)
+        return coords
+
+    def populate_property_from_node(self, node, feature, name, property=None):
+        try:
+            subnode = node.getElementsByTagName(name)[0]
+        except IndexError:
+            pass
+        else:
+            feature.properties[name] = subnode.firstChild.nodeValue
